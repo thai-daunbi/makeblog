1932dc336ab805e8b7617b8dee7a9dc1903e4a1d
leo.daunbi <leothai.daunbi@gmail.com>
2023-04-25 18:05:55 +0900

    Setting up sample site

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/getUser/{id}', [UserController::class, 'getUser']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'api'], function () {
+    Route::get('getUser', 'UserController@index');
+    Route::post('getUser', 'UserController@store');
+    Route::get('getUser/{id}', 'UserController@show');
+    Route::patch('getUser/{id}', 'UserController@update');
+    Route::delete('getUser/{id}', 'UserController@destroy');
 });
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,18 +1,36 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+
+/** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return app()->version();
 });
+
+$router->group(['prefix' => 'api/'], function () use ($router) {
+    $router->get('getUser', 'UserController@index');
+    $router->post('user', 'UserController@store');
+    $router->get('getUser/{id}', 'UserController@show');
+    $router->patch('getUser/{id}', 'UserController@update');
+    $router->delete('getUser/{id}', 'UserController@destroy');
+});
+
+// Route::group(['prefix' => 'api'], function () {
+//     // api.php 파일의 모든 라우트를 추가합니다.
+//     require base_path('routes/api.php');
+// });
+
+// // 나머지 웹 애플리케이션 라우트를 추가합니다.
+// Route::get('/', function () {
+//     return view('welcome');
+// });
