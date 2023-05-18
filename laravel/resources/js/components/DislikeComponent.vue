12644d4950ab5767e6aa7a7c1db4bdc44d7dbb93
leo.daunbi <leothai.daunbi@gmail.com>
2023-05-18 14:12:17 +0900

    edit-dislike-button(좋아요 버튼을 하나만 남기거나 버튼 하나만 누를 수 있도록+누르면 아이콘 변화 예정)

--- laravel/resources/js/components/DislikeComponent.vue
+++ laravel/resources/js/components/DislikeComponent.vue
@@ -1,47 +1,47 @@
 <template>
-    <div class="container">
-        <div id="success" class="mb-3"></div>
-        
-       <a style="cursor: pointer" @click.prevent="dislikeBlog">
-           <i class="fa fa-thumbs-o-down" aria-hidden="true"></i> 
-           ({{ alldislike }})
-       </a>
-    </div>
+  <div class="container">
+      <div id="success" class="mb-3"></div>
+      
+     <a style="cursor: pointer" @click.prevent="dislikeBlog">
+         <i class="fa fa-thumbs-o-down" aria-hidden="true"></i> 
+         ({{ alldislike }})
+     </a>
+  </div>
 </template>
 <script>
 import axios from "axios";
-  export default {
-      props:['blog'],
-      data(){
-          return {
-              alldislike:'',
-          }
-      },
-      methods:{
-          dislikeBlog(){
-              axios.post('/dislike/'+this.blog, {post: this.blog})
-              .then(res =>{
-                this.alldislike = parseInt(this.alldislike) + 1;
-                $('#success').html(res.data.message)
-              })
-              .catch(error => {
-                    console.log(error.message);
-                })
-          },
-          renderDislike(){
-            axios.post('/dislike', {post:this.blog})
+export default {
+    props:['blog'],
+    data(){
+        return {
+            alldislike:'',
+        }
+    },
+    methods:{
+        dislikeBlog(){
+            axios.post('/dislike/'+this.blog, {post: this.blog})
             .then(res =>{
-              if (res.data.post === null) {
-                // 처리할 코드 추가
-                console.log("post is null");
-                return;
-              }
-              this.alldislike = res.data.post.dislike;
+              $('#success').html(res.data.message);
+            // 좋아요 수를 업데이트하기 위해 fetchLikes 메서드 호출
+            this.fetchDislike();
             })
+            .catch(error => {
+                  console.log(error.message);
+              })
+        },
+        fetchDislike() {
+      axios.post('/dislike', { post: this.blog })
+        .then(res => {
+          if (res.data.post === null) {
+            console.log("post is null");
+            return;
           }
-      },
-      mounted() {
-          this.renderDislike()
-      }
+          this.alldislike = res.data.post.dislike;
+        });
+    }
+  },
+  mounted() {
+    this.fetchDislike();
   }
+};
 </script>
--- laravel/resources/js/components/LikeComponent.vue
+++ laravel/resources/js/components/LikeComponent.vue
@@ -1,51 +1,51 @@
 <template>
     <div class="container">
-      <div id="success" class="mb-3"></div>
-      <a style="cursor: pointer" @click.prevent="likeBlog">
-        <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
-        ({{ alllikes }})
-      </a>
+        <div id="success" class="mb-3"></div>
+        <a style="cursor: pointer" @click.prevent="likeBlog">
+            <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+            ({{ alllikes }})
+        </a>
     </div>
-  </template>
+</template>
   
-  <script>
-  import axios from "axios";
-  
-  export default {
+<script>
+import axios from "axios";
+
+export default {
     props: ['blog'],
     data() {
-      return {
-        alllikes: 0,
-      }
+        return {
+            alllikes: '',
+        }
     },
     methods: {
-      likeBlog() {
-        axios.post('/like/' + this.blog, {
-            post: this.blog
-          })
-          .then(res => {
-            $('#success').html(res.data.message);
-            // 좋아요 수를 업데이트하기 위해 fetchLikes 메서드 호출
-            this.fetchLikes();
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
-      fetchLikes() {
-        axios.post('/like', { post: this.blog })
-          .then(res => {
-            if (res.data.post === null) {
-              console.log("post is null");
-              return;
-            }
-            this.alllikes = res.data.post.like;
-          });
-      }
+        likeBlog() {
+            axios.post('/like/' + this.blog, {
+                post: this.blog
+            })
+                .then(res => {
+                    $('#success').html(res.data.message);
+                    // 좋아요 수를 업데이트하기 위해 fetchLikes 메서드 호출
+                    this.fetchLikes();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+        fetchLikes() {
+            axios.post('/like', { post: this.blog })
+                .then(res => {
+                    if (res.data.post === null) {
+                        console.log("post is null");
+                        return;
+                    }
+                    this.alllikes = res.data.post.like;
+                });
+        }
     },
     mounted() {
-      this.fetchLikes();
+        this.fetchLikes();
     }
-  };
-  </script>
+};
+</script>
   
